956aa52b41c88c13230cc69b3d47983111c260e2
GermanoCesarSS <germanosantos.cesar@gmail.com>
2022-12-03 22:04:03 -0300

    Dias, Horas, Minutos, Segundos

--- Studies_02/Studies_02/Program.cs
+++ Studies_02/Studies_02/Program.cs
@@ -7,20 +7,43 @@ namespace curso
     {
         static void Main(string[] args)
         {
+
+            int dias, horas, min, seg,
+                dias2, horas2, min2, seg2,
+                w, x, y, z, resto;
+
             string[] vet = Console.ReadLine().Split(' ');
-            int cod1 = int.Parse(vet[0]);
-            int uni1 = int.Parse(vet[1]);
-            double preco = double.Parse(vet[2], CultureInfo.InvariantCulture);
+            dias = int.Parse(vet[1]);
 
+            vet = Console.ReadLine().Split(' ');
+            horas = int.Parse(vet[0]);
+            min = int.Parse(vet[2]);
+            seg = int.Parse(vet[4]);
 
-            string[] vet2 = Console.ReadLine().Split(' ');
-            int cod2 = int.Parse(vet2[0]);
-            int uni2 = int.Parse(vet2[1]);
-            double preco2 = double.Parse(vet2[2], CultureInfo.InvariantCulture);
+            vet = Console.ReadLine().Split(' ');
+            dias2 = int.Parse(vet[1]);
 
-            double total = uni1 * preco + uni2 * preco2;
+            vet = Console.ReadLine().Split(' ');
+            horas2 = int.Parse(vet[0]);
+            min2 = int.Parse(vet[2]);
+            seg2 = int.Parse(vet[4]);
 
-            Console.WriteLine("VALOR A PAGAR: R$ " + total.ToString(("F2"), CultureInfo.InvariantCulture));
+            int inicio = (dias - 1) * 24 * 60 * 60 + horas * 60 * 60 + min * 60 + seg;
+            int fim = (dias2 - 1) * 24 * 60 * 60 + horas2 * 60 * 60 + min2 * 60 + seg2;
+
+            int duracao = fim - inicio;
+
+            w = duracao / (24 * 60 * 60);
+            resto = duracao % (24 * 60 * 60);
+            x = resto / (60 * 60);
+            resto = resto % (60 * 60);
+            y = resto / 60;
+            z = resto % 60;
+
+            Console.WriteLine(w + " dia(s)");
+            Console.WriteLine(x + " hora(s)");
+            Console.WriteLine(y + " minuto(s)");
+            Console.WriteLine(z + " segundo(s)");
 
         }
     }
